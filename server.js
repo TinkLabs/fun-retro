e13f6161dc908c10e24a0b039afb27e8c562995f
jack.xie <jack.xie@tinklabs.com>
2018-12-12 00:56:31 +0800

    specify path to app for alb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./dist'));
+app.use('/retroapp', express.static('./dist'));
 app.use('/health', function(req, res) {
     res.send({
         status: true
