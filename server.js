36b164b8d7d541802ceb24c515dc2771944ffa2b
jack.xie <jack.xie@tinklabs.com>
2018-12-12 01:34:31 +0800

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use('/retroapp', express.static('./dist'));
 app.use('/health', function(req, res) {
